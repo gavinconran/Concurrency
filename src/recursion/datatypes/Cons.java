371ba15bb00f90c6e4540aace58cd23d5a3d5c7a
gavin <gavinconran@yahoo.co.uk>
2016-11-26 21:33:18 +0000

    added missing @override annotations

--- src/recursion/datatypes/Cons.java
+++ src/recursion/datatypes/Cons.java
@@ -1,6 +1,7 @@
 package recursion.datatypes;
 
 public class Cons<E> implements ImList<E> {
+    
     private final E e;
     private final ImList<E> rest;
     private int size = 0;
@@ -13,25 +14,35 @@ public class Cons<E> implements ImList<E> {
         this.rest = rest;
         
     }
+    
+    @Override
     public ImList<E> cons(E e) {
         return new Cons<>(e, this);
     }
+    
+    @Override
     public E first() {
         return e;
     }
+    
+    @Override
     public ImList<E> rest() {
         return rest;
     }
     
+    @Override
     public int size() { 
         if (size == 0) size = 1 + rest.size();
         return size;
     }
     
+    @Override
     public boolean isEmpty() { return false; }
     
+    @Override
     public boolean contains(E e) { return this.e.equals(e) || rest.contains(e); }
     
+    @Override
     public E get(int n) { 
         if (n == 0)
             return first();
@@ -39,8 +50,10 @@ public class Cons<E> implements ImList<E> {
             return rest.get(n - 1);
     }
     
+    @Override
     public ImList<E> append(ImList<E> other) { return new Cons<>(e, rest.append(other)); }
     
+    @Override
     public ImList<E> reverse() { return rest.reverse().append(new Cons<>(e, new Empty<E>())); }
     
 }
